fbc63766031ce760583c016cea786470803ca353
RYZ-1331 <ryz_neko_a@icloud.com>
2026-04-02 13:34:59 +0900

    alpha-1.1.12
    alpha-1.1.12

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -18,7 +18,7 @@ const Users = [
     {key : "0604&a0604", bd : "0604", name : "ayano"},
     {key : "0711&a0711", bd : "0711", name : "aoi"},
     {key : "0712&k0712", bd : "0712", name : "kisuke"},
-    {key : "0813&t0813", bd : "0813", name : "tsukasa"},
+    {key : "0813&510510", bd : "0813", name : "tsukasa"},
     {key : "1202&n1202", bd : "1202", name : "nico"},
     {key : "1220&m1220", bd : "1220", name : "miina"}
 ]
